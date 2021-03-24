fef722bb75d2876ae4f901c1f9f848f7a2da6b35
Lezhnin Ilya <ilya-dodge@mail.ru>
2021-03-24 22:22:34 +0700

    Создал класс CurrentTableViewCell создал визуализацию кастовой ячейки на которой разместил imageView и три lable создал отлёты для добавленных объектов ячейки переделал заполнение ячейки с использованиям кастовой ячейки

---
+++ MyFavoritePlacies/CurrentTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentTableViewCell.swift
+//  MyFavoritePlacies
+//
+//  Created by Ilya Lezhnin on 24.03.2021.
+//
+
+import UIKit
+
+class CurrentTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+
+}
